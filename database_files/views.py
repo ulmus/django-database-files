500d6b0e3fce2fe77a5f246ea5dd0ceb21485be6
jensa <jensa@10.0.1.5>
2010-11-27 18:32:32 +0100

    Fix unidecode of non-ascii filenames if the unidecode module is installed

--- database_files/views.py
+++ database_files/views.py
@@ -3,12 +3,19 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.cache import cache_control
 import mimetypes
 from database_files.models import DatabaseFile
+from django.template.defaultfilters import slugify
 
 @cache_control(max_age=86400)
 def serve(request, file_id):
 	f = get_object_or_404(DatabaseFile, pk=file_id)
-	mimetype = mimetypes.guess_type(f.get_filename())[0] or 'application/octet-stream'
+	safe_filename = f.get_filename()
+	try:
+		import unidecode
+		safe_filename = unidecode.unidecode(safe_filename)
+	except ImportError:
+		pass
+	mimetype = mimetypes.guess_type(safe_filename)[0] or 'application/octet-stream'
 	response = HttpResponse(f.retreive(), mimetype=mimetype)
 	response['Content-Length'] = f.size
-	response['Content-Disposition'] = "filename=%s" % f.get_filename()
+	response['Content-Disposition'] = "filename=%s" % safe_filename
 	return response
